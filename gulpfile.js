50f1602be48ac38977594adfe5e59261c2ab364f
jacky.wu <jacky_coder@163.com>
2018-04-04 17:38:19 +0800

    Update 2018-04-04T09:38:16.527Z

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var ghPages = require("gulp-gh-pages");
 
 gulp.task('deploy', function () {
   return gulp.src("./_book/**/*.*")
-    .pipe(ghPages())
+    .pipe(ghPages({
+      branch: 'master'
+    }))
     .on("error", function(err){
       console.log(err)
     });
